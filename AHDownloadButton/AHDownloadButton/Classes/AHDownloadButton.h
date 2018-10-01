bb01f59081f1a39a24fb14c04343b70eb7488f31
Miwand <miwand.najafe@gmail.com>
2018-09-30 21:59:42 -0400

    Adding Carthage support

---
+++ AHDownloadButton/AHDownloadButton/Classes/AHDownloadButton.h
@@ -0,0 +1,19 @@
+//
+//  AHDownloadButton.h
+//  AHDownloadButton
+//
+//  Created by Miwand Najafe on 2018-09-30.
+//  Copyright © 2018 Miwand Najafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AHDownloadButton.
+FOUNDATION_EXPORT double AHDownloadButtonVersionNumber;
+
+//! Project version string for AHDownloadButton.
+FOUNDATION_EXPORT const unsigned char AHDownloadButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AHDownloadButton/PublicHeader.h>
+
+
